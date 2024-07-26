846e5daef53c1a1f84ca9340cb129481204fd095
oddster08 <ayushguptaa20@gmail.com>
2024-07-27 03:03:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { firebaseConfig } from './firebaseConfig';
+import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'medProject';
+  constructor(){
+    initializeApp(firebaseConfig);
+  }
 }
---
+++ src/app/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC4DV4cpNNv2jR_BunYty6HVuNT50gacdU",
+    authDomain: "codebin-ng-6afbd.firebaseapp.com",
+    projectId: "codebin-ng-6afbd",
+    storageBucket: "codebin-ng-6afbd.appspot.com",
+    messagingSenderId: "88639350687",
+    appId: "1:88639350687:web:dc6202816bb95ce82f3faa",
+    measurementId: "G-XS6XY0126L"
+  };
